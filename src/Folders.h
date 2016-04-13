c5f22921091a371e983c1f5230d4c14296c2f669
martapips <ei12106@fe.up.pt>
2016-04-13 22:51:45 +0100

    ta quase hehe

---
+++ src/Folders.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <string>
+
+namespace Folders {
+
+const std::string DATA_FOLDER = "../ck-data/";
+const std::string TRAIN_DATA_PATH = DATA_FOLDER + "rawdata/train-data.txt";
+const std::string TEST_DATA_PATH = DATA_FOLDER + "rawdata/test-data.txt";
+
+}
---
+++ src/Main.cpp
@@ -0,0 +1,36 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+
+#include "Folders.h"
+
+int main() {
+	// https://www.rulequest.com/see5-unix.html
+	std::string input = Folders::DATA_FOLDER + "input/ck";
+	std::string options = "";
+
+	//-b -g -w
+	//-b -p
+	//-b -p -w
+	//-b
+	//-b -w
+	//-g
+	//-p
+	//-p -w
+	//-w
+	//-X 100
+	//-X 10
+	//noflags
+
+	std::string output = Folders::DATA_FOLDER + "output/" + "\"" + options + "_results.txt\"";
+
+	std::string cmd = "../c50/c5.0 -f " + input + " " + options + " > " + output;
+
+	system(cmd.c_str());
+
+	std::cout << "Done." << std::endl;
+
+	return 0;
+}
+
+
